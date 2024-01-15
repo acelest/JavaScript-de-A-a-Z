d1001f838fd95f079a38d0858ab87ad57bd6e665
Acelest <eyengadjou@gmail.com>
2024-01-15 12:45:54 +0100

    les types de valeurs en js

---
+++ 03-type-valeur/app.js
@@ -0,0 +1,31 @@
+//string
+
+let txt = "Lions";
+
+//number
+
+let nv = 2;
+let nb2 = 2.5;
+
+// boolen
+
+let bool = false;
+
+// tableau
+
+let tab = [1, 2, , 3, 4, 5];
+let tab2 = ["Jonathan", "Marie"];
+
+//les bojects
+
+let objet = {
+  marque: "City",
+  annee: 2015,
+};
+
+// undefined
+let und;
+
+// null
+
+let a = null;
