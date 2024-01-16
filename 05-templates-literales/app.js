69cf0ee42621b061a81dcdbe98ac9b1d40a14a52
Acelest <eyengadjou@gmail.com>
2024-01-16 21:36:32 +0100

    les templates literals

--- 05-templates-literales/app.js
+++ 05-templates-literales/app.js
@@ -2,6 +2,7 @@ const prenom = "Jonathan";
 const age = 30;
 const job = "developpeur";
 
-const personage = "Je suis " + prenom + "et j'ai " + age + "je suis " + job;
+// const personage = "Je suis " + prenom + "et j'ai " + age + "je suis " + job;
+const personage = `je suis ${prenom}, j'ai ${age} et je suis ${job}`;
 
-console.log;
+console.log(personage);
