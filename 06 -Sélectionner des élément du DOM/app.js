8f1003f520713a0fd5e7c0b31a1788020c6d9b04
Acelest <eyengadjou@gmail.com>
2024-01-16 22:12:38 +0100

    6. Sélectionner des élément du DOM

---
+++ 06 -Sélectionner des élément du DOM/app.js
@@ -0,0 +1,11 @@
+const prenom = "Aubin";
+const age = 19;
+const job = "developpeur";
+
+//novice
+const personnage = "Je suis " + prenom + "et j'ai " + age + "je suis " + job;
+
+//senior
+const personnage1 = `je suis ${prenom}, j'ai ${age} et je suis ${job}`;
+
+console.log(personnage);
