eaa8c1c4094bee3c7aa41f0330849ef632385c71
Acelest <eyengadjou@gmail.com>
2024-01-16 21:30:16 +0100

    templates literals

---
+++ 05-templates-literales/app.js
@@ -0,0 +1,32 @@
+let nb1 = 5;
+let nb2 = 54;
+
+//addition
+
+let nb3 = nb1 + nb2;
+
+console.log(nb3);
+
+//multitplication
+
+let mul = nb1 * nb2;
+
+console.log(mul);
+
+//soustraction
+
+let sous = nb1 - nb2;
+
+console.log(sous);
+
+//division
+
+let div = nb1 / nb2;
+
+console.log(div);
+
+//modulo = le reste d'une division
+
+let modulo = nb1 % nb2;
+
+console.log(modulo);
