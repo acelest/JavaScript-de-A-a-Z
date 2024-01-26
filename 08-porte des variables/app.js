031c72bb1efb607ad58f26e6404d3e4d4dfa3f81
Acelest <eyengadjou@gmail.com>
2024-01-26 12:24:22 +0100

    porte des variables

---
+++ 08-porte des variables/app.js
@@ -0,0 +1,13 @@
+
+// var sont disponible dans l'entierete du code;
+// let et const sont  pas disponible dans l'entierete du code 
+
+let nom = 'Djou'
+
+function pren() {
+  return nom
+}
+
+console.log(pren());
+
+console.log(window);
