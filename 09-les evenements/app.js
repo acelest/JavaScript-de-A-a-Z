458b26cb1c36830ec1ce3a32953a434c117bd77c
Acelest <eyengadjou@gmail.com>
2024-01-26 12:24:39 +0100

    les evenements

---
+++ 09-les evenements/app.js
@@ -0,0 +1,14 @@
+const box = document.querySelector('.box');
+
+
+document.addEventListener('mouseleave', ()=> {
+  box.classList.add('active')
+})
+
+// click = qaund je clique 
+// dblclick = quand je clique deux fois rapidement
+// load 
+// keydown ou keypress
+// mouseout, mouseenter, mouseover, mouseleave
+
+ 
