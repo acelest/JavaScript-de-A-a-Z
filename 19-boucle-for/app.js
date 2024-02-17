d0757316e89d516d832fa52ccd9768bdbed42552
Aubin <eyengadjou@gmail.com>
2024-02-17 16:17:39 +0100

    for

---
+++ 19-boucle-for/app.js
@@ -0,0 +1,14 @@
+// Boucle For
+
+// la boucle for s'est a iterer a travers un tb ou object
+
+
+// for (i = 0; i < 0; i++ ){
+// }
+
+let compteur = 100
+
+for ( i=0; i< compteur; i++) {
+  console.log(i);
+
+}
