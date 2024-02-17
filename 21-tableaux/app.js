6d3e15cef69f3bfde158e8de31c113b2851d6dce
Aubin <eyengadjou@gmail.com>
2024-02-17 21:57:39 +0100

    tableaux

---
+++ 21-tableaux/app.js
@@ -0,0 +1,20 @@
+const tab = ['aubin','mathieu', 'lili' ]
+
+const tab2 =  ['aubin', 3, {marque:'tesla', anne:2025}]
+
+console.log(tab);
+console.log(tab2);
+
+// acceder a une valeur d'un tab
+
+console.log( tab[0]);
+
+// changer la valeur d'un index 
+tab2[0] = "nuil"
+console.log(tab2)
+
+
+// length = longeur du tableaux
+console.log(tab.length);
+
+// at = obtenir une valeur d'un tab
