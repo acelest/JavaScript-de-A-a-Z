008498cb50a6494ea0a71f8d26f97fe3ee08e4fa
Aubin <eyengadjou@gmail.com>
2024-02-17 14:33:28 +0100

    if else main js

---
+++ 16-if-else/app.js
@@ -0,0 +1,36 @@
+let nb = 10 ;
+// les conditions simples
+// if(nb <5) {
+//   console.log('il est inferieur a 5');
+
+// }  else if (nb> 5 ) {
+// console.log('il est superieur a 5');
+// } else if (nb === 5) {
+//   console.log('il est egale ');
+// } else {
+//   console.log('error')
+// }
+
+// les conditions composees 
+
+// Ou Logique
+if (nb < 5 || nb ===5 ) {
+console.log('condition 1 respecte');
+
+} else if (nb > 5){
+  console.log('condition 2 respecte');
+}
+ else {
+  console.log('condition par defaut');
+ }
+ 
+// Et logique
+ if (nb < 5 || nb ===5 ) {
+  console.log('condition 1 respecte');
+  
+  } else if (nb > 5){
+    console.log('condition 2 respecte');
+  }
+   else {
+    console.log('condition par defaut');
+   }
